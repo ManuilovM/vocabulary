5aae46040dd2762499a959bef283e0789ab391e2
My Name <Vsadnick95@yandex.ru>
2021-04-26 10:30:01 +0300

    generated TopBarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
 
   ],
   imports: [
