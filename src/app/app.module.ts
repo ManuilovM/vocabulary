f48225395933976504084c7479b63880a20eb853
My Name <Vsadnick95@yandex.ru>
2021-04-26 11:37:20 +0300

    g homePageComponent, layout HomePageComponrnt; layout TopBarComponent;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    HomePageComponent,
 
   ],
   imports: [
